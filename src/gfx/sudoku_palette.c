2e00d8323af0b5643484ff71590ce8ded82601a9
Jeffitus <jeffitus@gmail.com>
2020-08-23 15:59:39 -0500

    remove unnecessary files

--- src/gfx/sudoku_palette.c
+++
@@ -1,6 +0,0 @@
-unsigned char sudoku_palette[6] =
-{
-    0x54, 0x89, /*   0: rgb( 16,  85, 165) */
-    0x00, 0x00, /*   1: rgb(  0,   0,   0) */
-    0xff, 0xff, /*   2: rgb(255, 255, 255) */
-};
